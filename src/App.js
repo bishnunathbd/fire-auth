c457bb0afac6744f98b07564d86bc74e0ca09918
bishnunathbd <bishnunath774@gmail.com>
2021-04-11 20:38:04 +0600

    41-5-firebase setup done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+
+// Initialize Firebase
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 function App() {
   return (
