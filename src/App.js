b6190eb0ff7057e587b8ebb29d9dc4bf15925d8f
bishnunathbd <bishnunath774@gmail.com>
2021-04-12 16:44:19 +0600

    42-2-form field validation

--- src/App.js
+++ src/App.js
@@ -61,7 +61,16 @@ function App() {
   }
 
   const handleBlur = (e) => {
-    console.log(e.target.name, e.target.value);
+    // console.log(e.target.name, e.target.value);
+    if (e.target.name === 'email') {
+      const isEmailValid = /\S+@\S+\.\S+/.test(e.target.value)
+      console.log(isEmailValid);
+    }
+    if (e.target.name === 'password') {
+      const passwordLength = e.target.value.length > 6;
+      const passwordHasNumber = /\d{1}/.test(e.target.value)
+      console.log(passwordLength && passwordHasNumber);
+    }
   }
 
   const handleSubmit = () => {
